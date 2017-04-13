40b3c37cd8891b73de7018c4a89b0f89eb35624f
Noah Keating <noah@quirky.com>
2017-04-13 14:58:39 -0400

    adds tint color

--- Sources/MultiToggleButton.swift
+++ Sources/MultiToggleButton.swift
@@ -13,7 +13,7 @@ public typealias ToggleButton = MultiToggleButton // compatibility with old vers
 open class MultiToggleButton: UIButton
 {
     /// use only this init, it's 'convenience' only to avoid overriding required inits
-    public convenience init(images: [UIImage?], states: [String], colors: [UIColor?] = [], bgColors: [UIColor?] = [], action: ((_ sender: MultiToggleButton) -> ())? = nil) {
+    public convenience init(images: [UIImage?], states: [String], tints: [UIColor?] = [], colors: [UIColor?] = [], bgColors: [UIColor?] = [], action: ((_ sender: MultiToggleButton) -> ())? = nil) {
         self.init(frame: CGRect.zero)
         
         if let image = images.first {
@@ -23,6 +23,7 @@ open class MultiToggleButton: UIButton
         
         self.images   = images
         self.states   = states
+        self.tints    = tints
         self.colors   = colors
         self.bgColors = bgColors
         self.action   = action
@@ -31,8 +32,8 @@ open class MultiToggleButton: UIButton
         setupCurrentState()
     }
     
-    public convenience init(image: UIImage?, states: [String], colors: [UIColor?] = [], bgColors: [UIColor?] = [], action: ((_ sender: MultiToggleButton) -> ())? = nil) {
-        self.init(images: [image], states: states, colors: colors, bgColors: bgColors, action: action)
+    public convenience init(image: UIImage?, states: [String], tints: [UIColor?] = [], colors: [UIColor?] = [], bgColors: [UIColor?] = [], action: ((_ sender: MultiToggleButton) -> ())? = nil) {
+        self.init(images: [image], states: states, tints: tints, colors: colors, bgColors: bgColors, action: action)
     }
     
     // MARK: - Manual Control
@@ -43,6 +44,7 @@ open class MultiToggleButton: UIButton
     }
     
     open var currentStateIndex: Int = 0      { didSet {setupCurrentState()} }
+    open var tints:  [UIColor?] = []         { didSet {setupCurrentState()} }
     open var colors: [UIColor?] = []         { didSet {setupCurrentState()} }
     open var bgColors: [UIColor?] = []       { didSet {setupCurrentState()} }
     open var images: [UIImage?] = []         { didSet {setupCurrentState()} }
@@ -73,6 +75,7 @@ open class MultiToggleButton: UIButton
         setTitleColor(currentColor ?? tintColor, for: UIControlState())
         self.backgroundColor = currentBGColor ?? tintColor
         setImage(currentToggleImage ?? currentImage, for: UIControlState())
+        self.image.tintColor = currentTintColor ?? tintColor
     }
     
     private var currentColor: UIColor? {
@@ -85,5 +88,9 @@ open class MultiToggleButton: UIButton
     
     private var currentToggleImage: UIImage? {
         return currentStateIndex < images.count ? images[currentStateIndex] : nil
+    }
+    
+    private var currentTintColor: UIColor? {
+        return currentStateIndex < tints.count ? tints[currentStateIndex] : nil
     }
 }
